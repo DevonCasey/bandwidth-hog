2d9a12928bb187ada5ffa887c1693152724e4a5d
DevonCasey <devon.s.casey@gmail.com>
2023-04-21 14:30:38 -0400

    finalize

--- bandwidth-hog-viz.py
+++ bandwidth-hog-viz.py
@@ -15,17 +15,19 @@ columns = ['Hostname',
            'NIC8', 'BandwidthNIC8',
            'Time', 'Date'] # Bandwidth is in MB
 filename = askopenfilename()
-df = pd.read_csv(filename , usecols=columns)
-hostname = df.Hostname[1]
+df = pd.read_csv(filename , usecols=columns) # Reads the csv selected above into a pandas dataframe 
+hostname = df.Hostname[1] # Creates a variable to use for labeling 
 print('Selected: ' + filename)
-df = df.tail(-1) # there is a null row at the top of the table, lets delete it.
+df = df.tail(-1) # There is a null row at the top of the table, lets delete it.
+df = df.fillna(0)
+df
 
-x_ticks = 250 # a tick every 250 cycles
-y_ticks = 5 # a tick every 5 MB
-fig = plt.figure(figsize=(20, 4))
+x_ticks = 250 # A tick every 250 cycles
+y_ticks = 5 # A tick every 5 MB
+fig = plt.figure(figsize=(20, 4)) # Creates a figure with size 20 x 4.
 ax = fig.add_subplot(111)
-ax.plot(df.Time, df.BandwidthNIC1, label=hostname)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(x_ticks)) # set the ticks
+ax.plot(df.Time, df.BandwidthNIC1, label=hostname) # Uses the label created earlier
+ax.xaxis.set_major_locator(ticker.MultipleLocator(x_ticks))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(y_ticks))
 plt.legend()
 plt.show()
